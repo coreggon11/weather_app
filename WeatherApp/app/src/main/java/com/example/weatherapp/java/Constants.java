fd3f491d10a3f10e920a165dc2ed60b2b6037ae3
Dominik Krížo <dominik.krizo>
2020-08-07 22:46:49 +0200

    getting celcius units

--- WeatherApp/app/src/main/java/com/example/weatherapp/java/Constants.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/java/Constants.java
@@ -3,7 +3,7 @@ package com.example.weatherapp.java;
 public class Constants {
 
     public static final String API_KEY = "6c5b345608698b4bab1083ef8e2cbe8d";
-    public static final String API_CALL = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s";
+    public static final String API_CALL = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric";
 
     public static String getApiCall(String city) {
         return String.format(API_CALL, city, API_KEY);
